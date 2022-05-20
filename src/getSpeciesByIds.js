d170550eb1fff737fa606201b264a0ac0592f9e3
Alano <alanosantosdev@gmail.com>
2022-05-20 18:41:07 -0300

    requisito 1 correção lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -7,6 +7,4 @@ function getSpeciesByIds(...ids) {
   }
   return species.filter((elemento) => ids.includes(elemento.id));
 }
-
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d'));
 module.exports = getSpeciesByIds;
