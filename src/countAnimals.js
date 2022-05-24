92d42b47ca79a515062e733ad02976c63ac6621e
Alano <alanosantosdev@gmail.com>
2022-05-23 23:45:15 -0300

    testes requisito 6 testando cobertura

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const semInput = species.reduce((acc, ell) => {
+      acc[ell.name] = ell.residents.length;
+      return (acc);
+    }, {});
+    return semInput;
+  }
+  const comInput = species.find((ell2) => {
+    if (Object.values(animal).includes(ell2.name)) {
+      return ell2;
+    }
+  });
+  return comInput.residents.length;
 }
+console.log(countAnimals({ species: 'lions' }));
 
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,16 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Sem receber nenhum parametro a função deve retornar "undefined" ', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Para o argumento names deve retornar um array de nomes que possui o nome Jefferson', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Para o argumento count deve retornar o número inteiro 4', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('Para o argumento averageAge deve retornar um número próximo a 10.5;', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+});
