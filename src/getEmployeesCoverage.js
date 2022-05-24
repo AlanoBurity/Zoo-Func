54be993589fc4d74b47d152a3a0a11343a514f0e
Alano <alanosantosdev@gmail.com>
2022-05-24 16:33:57 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const colaboradores = (name) => employees.filter((ell) =>
+  ell.firstName === name || ell.lastName === name || ell.id === name);
+const nomeAnimal = (animalId) => animalId.map((id) =>
+  species.find((ell) => ell.id === id).name);
+const animalLocalizacao = (animalId) => animalId.map((id) =>
+  species.find((ell) => ell.id === id).location);
+const infoColaboradores = (param) => {
+  const colaborador = colaboradores(Object.values(param)[0])[0];
+  return {
+    id: colaborador.id,
+    fullName: `${colaborador.firstName} ${colaborador.lastName}`,
+    species: nomeAnimal(colaborador.responsibleFor),
+    locations: animalLocalizacao(colaborador.responsibleFor),
+  };
+};
+
+const allInfoArr = (obj) => obj.map((ell) => ({
+  id: ell.id,
+  fullName: `${ell.firstName} ${ell.lastName}`,
+  species: nomeAnimal(ell.responsibleFor),
+  locations: animalLocalizacao(ell.responsibleFor),
+}));
+
+const getEmployeesCoverage = (param) => {
+  if (!param) {
+    return allInfoArr(employees);
+  }
+  if (colaboradores(Object.values(param)[0]).length < 1) {
+    throw new Error('Informações inválidas');
+  }
+  return infoColaboradores(param);
+};
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
