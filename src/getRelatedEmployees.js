586cc0d4934f2be69ecb8ab15849d51ece7321c2
Alano <alanosantosdev@gmail.com>
2022-05-23 21:11:39 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.filter((funcionarios) => funcionarios.id === id)
+    .every((elemento) => {
+      if (elemento.managers.length === 0 || elemento.managers.length === 1) {
+        return true;
+      } return false;
+    });
 }
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees
+    .filter((empregados) => (empregados.managers.includes(managerId)))
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
 }
-
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
